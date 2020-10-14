ea1cf82598206c18d783575af9e89991d348767e
Burak Köken <burakkokenn@gmail.com>
2020-10-14 21:55:43 +0200

    Created the interface Type and struct BaseType. In addition, added some util methods which will help you while developing the other parts.

---
+++ type.go
@@ -0,0 +1,99 @@
+package goo
+
+import (
+	"reflect"
+)
+
+type Type interface {
+	GetName() string
+	GetFullName() string
+	GetPackageName() string
+	GetPackageFullName() string
+	GetGoType() reflect.Type
+	GetGoValue() reflect.Value
+	IsFunction() bool
+	IsStruct() bool
+	IsInterface() bool
+	IsString() bool
+	String() string
+}
+
+type baseType struct {
+	name            string
+	packageName     string
+	packageFullName string
+	typ             reflect.Type
+	val             reflect.Value
+	kind            reflect.Kind
+}
+
+func newBaseType(typ reflect.Type, val reflect.Value) baseType {
+	return baseType{
+		getTypeName(typ, val),
+		getPackageName(typ, val),
+		getPackageFullName(typ, val),
+		typ,
+		val,
+		typ.Kind(),
+	}
+}
+
+func (typ baseType) GetName() string {
+	return typ.name
+}
+
+func (typ baseType) GetFullName() string {
+	return typ.packageFullName + "." + typ.name
+}
+
+func (typ baseType) GetPackageName() string {
+	return typ.packageName
+}
+
+func (typ baseType) GetPackageFullName() string {
+	return typ.packageFullName
+}
+
+func (typ baseType) GetGoType() reflect.Type {
+	return typ.typ
+}
+
+func (typ baseType) GetGoValue() reflect.Value {
+	return typ.val
+}
+
+func (typ baseType) IsFunction() bool {
+	return reflect.Func == typ.kind
+}
+
+func (typ baseType) IsStruct() bool {
+	return reflect.Struct == typ.kind
+}
+
+func (typ baseType) IsInterface() bool {
+	return reflect.Interface == typ.kind
+}
+
+func (typ baseType) IsString() bool {
+	return reflect.String == typ.kind
+}
+
+func (typ baseType) String() string {
+	return typ.name
+}
+
+func GetType(obj interface{}) Type {
+	typ, val := GetGoTypeAndValue(obj)
+	return createBaseType(typ, val)
+}
+
+func GetTypeFromGoType(typ reflect.Type) Type {
+	if typ == nil {
+		panic("Type cannot be nil")
+	}
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+	baseTyp := newBaseType(typ, reflect.Value{})
+	return getActualTypeFromBaseType(baseTyp)
+}
---
+++ util.go
@@ -0,0 +1,138 @@
+package goo
+
+import (
+	"reflect"
+	"runtime"
+	"strings"
+	"unicode"
+)
+
+func sanitizedName(str string) string {
+	name := strings.ReplaceAll(str, "/", ".")
+	name = strings.ReplaceAll(name, "-", ".")
+	name = strings.ReplaceAll(name, "_", ".")
+	return name
+}
+
+func getActualTypeFromBaseType(baseTyp baseType) Type {
+	if baseTyp.IsFunction() {
+		return nil
+	} else if baseTyp.IsInterface() {
+		return nil
+	} else if baseTyp.IsStruct() {
+		return nil
+	}
+	return baseTyp
+}
+
+func createBaseType(typ reflect.Type, val reflect.Value) baseType {
+	return newBaseType(typ, val)
+}
+
+func getTypeName(typ reflect.Type, val reflect.Value) string {
+	defer func() {
+		if r := recover(); r != nil {
+		}
+	}()
+	switch typ.Kind() {
+	case reflect.Struct:
+		return getBaseTypeName(typ)
+	case reflect.Interface:
+		return getBaseTypeName(typ)
+	case reflect.Func:
+		return getFunctionName(val)
+	}
+	return ""
+}
+
+func GetGoTypeAndValue(obj interface{}) (reflect.Type, reflect.Value) {
+	typ := reflect.TypeOf(obj)
+	if typ == nil {
+		panic("Type cannot be determined as the given object is nil")
+	}
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+	val := reflect.ValueOf(obj)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	return typ, val
+}
+
+func getBaseTypeName(typ reflect.Type) string {
+	name := typ.Name()
+	if name == "" {
+		name = typ.String()
+	}
+	return name
+}
+
+func getPackageName(typ reflect.Type, val reflect.Value) string {
+	defer func() {
+		if r := recover(); r != nil {
+		}
+	}()
+	if reflect.Func == typ.Kind() {
+		return getFunctionPackageName(val)
+	}
+	dotLastIndex := strings.LastIndex(typ.String(), ".")
+	if dotLastIndex != -1 {
+		return typ.String()[:dotLastIndex]
+	}
+	return ""
+}
+
+func getPackageFullName(typ reflect.Type, val reflect.Value) string {
+	defer func() {
+		if r := recover(); r != nil {
+		}
+	}()
+	if reflect.Func == typ.Kind() {
+		return getFunctionPackageFullName(val)
+	}
+	return sanitizedName(typ.PkgPath())
+}
+
+func getStructOrInterfaceFullName(typ reflect.Type) string {
+	defer func() {
+		if r := recover(); r != nil {
+		}
+	}()
+	return sanitizedName(typ.PkgPath()) + "." + getBaseTypeName(typ)
+}
+
+func getFunctionName(val reflect.Value) string {
+	fullName := runtime.FuncForPC(val.Pointer()).Name()
+	dotLastIndex := strings.LastIndex(fullName, ".")
+	if dotLastIndex != -1 {
+		fullName = fullName[dotLastIndex+1:]
+	}
+	return fullName
+}
+
+func getFunctionPackageFullName(val reflect.Value) string {
+	fullName := runtime.FuncForPC(val.Pointer()).Name()
+	dotLastIndex := strings.LastIndex(fullName, ".")
+	if dotLastIndex != -1 {
+		fullName = fullName[:dotLastIndex]
+	}
+	return sanitizedName(fullName)
+}
+
+func getFunctionPackageName(val reflect.Value) string {
+	fullName := getFunctionPackageFullName(val)
+	dotLastIndex := strings.LastIndex(fullName, ".")
+	if dotLastIndex != -1 {
+		fullName = fullName[dotLastIndex+1:]
+	}
+	return fullName
+}
+
+func isExportedField(structField reflect.StructField) bool {
+	return unicode.IsUpper(rune(structField.Name[0]))
+}
+
+func isExportedMethod(method reflect.Method) bool {
+	return unicode.IsUpper(rune(method.Name[0]))
+}
