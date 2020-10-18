8cc7db5733747eb43d423e8511efcbf3d09f5fa5
Burak Köken <burakkokenn@gmail.com>
2020-10-18 18:35:25 +0200

    Fixed the bug #1 The method Call in Function causes crashing when being called by nil parameters.

--- function.go
+++ function.go
@@ -50,9 +50,18 @@ func (fun functionType) GetFunctionReturnTypeCount() int {
 }
 
 func (fun functionType) Call(args []interface{}) []interface{} {
+	parameterCount := fun.GetFunctionParameterCount()
+	if (args == nil && parameterCount != 0) || (args != nil && len(args) == parameterCount) {
+		panic("Parameter counts don't match argument counts")
+	}
 	inputs := make([]reflect.Value, 0)
-	for _, arg := range args {
-		inputs = append(inputs, reflect.ValueOf(arg))
+	for index, arg := range args {
+		if arg == nil {
+			paramType := fun.GetFunctionParameterTypes()[index]
+			inputs = append(inputs, reflect.New(paramType.GetGoType()).Elem())
+		} else {
+			inputs = append(inputs, reflect.ValueOf(arg))
+		}
 	}
 	outputs := make([]interface{}, 0)
 	results := fun.val.Call(inputs)
