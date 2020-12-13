2652850125002ea3c930c6ddc11a47aa0aec8eef
Burak Köken <burakkokenn@gmail.com>
2020-12-13 21:07:11 +0200

    Updated the unit tests for struct.go

--- struct_test.go
+++ struct_test.go
@@ -2,103 +2,10 @@ package goo
 
 import (
 	"github.com/stretchr/testify/assert"
-	"log"
 	"testing"
 )
 
-type Run interface {
-	Run()
-}
-
-type Bark interface {
-	Bark()
-}
-
-type Animal struct {
-	Name string
-}
-
-func (animal Animal) SayHi() string {
-	return "Hi, I'm " + animal.Name
-}
-
-type Dog struct {
-	Animal
-}
-
-func (dog *Dog) Bark() {
-	log.Print("Bark")
-}
-
-func (dog Dog) Run() {
-	log.Print("Run")
-}
-
-type Person struct {
-	name    string
-	Surname string
-	age     int
-	Address Address
-}
-
-func (person Person) GetName() string {
-	return person.name
-}
-
-func (person Person) GetSurname() string {
-	return person.Surname
-}
-
-func (person Person) getAge() int {
-	return person.age
-}
-
-func (person *Person) GetAddress() Address {
-	return person.Address
-}
-
-type Address struct {
-	city    string
-	country string
-}
-
-func (address Address) GetCity() string {
-	return address.city
-}
-
-func (address Address) GetCountry() string {
-	return address.country
-}
-
-func TestIsMethods(t *testing.T) {
-	typ := GetType(Animal{})
-	assert.Equal(t, true, typ.IsStruct())
-	assert.Equal(t, false, typ.IsInterface())
-	assert.Equal(t, false, typ.IsFunction())
-	assert.Equal(t, false, typ.IsNumber())
-	assert.Equal(t, true, typ.IsInstantiable())
-	assert.Equal(t, false, typ.IsMap())
-	assert.Equal(t, false, typ.IsPointer())
-	assert.Equal(t, false, typ.IsArray())
-	assert.Equal(t, false, typ.IsString())
-	assert.Equal(t, false, typ.IsBoolean())
-	assert.Equal(t, false, typ.IsSlice())
-
-	typ = GetType(&Animal{})
-	assert.Equal(t, true, typ.IsStruct())
-	assert.Equal(t, false, typ.IsInterface())
-	assert.Equal(t, false, typ.IsFunction())
-	assert.Equal(t, false, typ.IsNumber())
-	assert.Equal(t, true, typ.IsInstantiable())
-	assert.Equal(t, false, typ.IsMap())
-	assert.Equal(t, true, typ.IsPointer())
-	assert.Equal(t, false, typ.IsArray())
-	assert.Equal(t, false, typ.IsString())
-	assert.Equal(t, false, typ.IsBoolean())
-	assert.Equal(t, false, typ.IsSlice())
-}
-
-func TestGetNamesForStruct(t *testing.T) {
+func TestStructType_GetNames(t *testing.T) {
 	testGetNamesForStruct(t, GetType(Animal{}))
 	testGetNamesForStruct(t, GetType(&Animal{}))
 }
@@ -111,7 +18,7 @@ func testGetNamesForStruct(t *testing.T, typ Type) {
 	assert.Equal(t, typ.(Struct), typ.ToStructType())
 }
 
-func TestGetFieldsForStruct(t *testing.T) {
+func TestStructType_GetFields(t *testing.T) {
 	testGetFieldsForStruct(t, GetType(Person{}))
 	testGetFieldsForStruct(t, GetType(&Person{}))
 }
@@ -120,15 +27,15 @@ func testGetFieldsForStruct(t *testing.T, typ Type) {
 	structType := typ.(Struct)
 	// all fields
 	fieldCount := structType.GetFieldCount()
-	assert.Equal(t, 4, fieldCount)
+	assert.Equal(t, 5, fieldCount)
 	fields := structType.GetFields()
-	assert.Equal(t, 4, len(fields))
+	assert.Equal(t, 5, len(fields))
 
 	// exported fields
 	fieldCount = structType.GetExportedFieldCount()
-	assert.Equal(t, 2, fieldCount)
+	assert.Equal(t, 3, fieldCount)
 	fields = structType.GetExportedFields()
-	assert.Equal(t, 2, len(fields))
+	assert.Equal(t, 3, len(fields))
 
 	// unexported fields
 	fieldCount = structType.GetUnexportedFieldCount()
@@ -138,12 +45,12 @@ func testGetFieldsForStruct(t *testing.T, typ Type) {
 
 	// anonymous fields
 	fieldCount = structType.GetAnonymousFieldCount()
-	assert.Equal(t, 0, fieldCount)
+	assert.Equal(t, 1, fieldCount)
 	fields = structType.GetAnonymousFields()
-	assert.Equal(t, 0, len(fields))
+	assert.Equal(t, 1, len(fields))
 }
 
-func TestGetMethodsForStruct(t *testing.T) {
+func TestStructType_GetStructMethods(t *testing.T) {
 	typ := GetType(Person{})
 	structType := typ.(Struct)
 	methodsCount := structType.GetStructMethodCount()
@@ -159,7 +66,7 @@ func TestGetMethodsForStruct(t *testing.T) {
 	assert.Equal(t, 3, len(methods))
 }
 
-func TestImplementsForStruct(t *testing.T) {
+func TestStructType_Implements(t *testing.T) {
 	x := &Dog{}
 	x.Run()
 	typ := GetType(Dog{})
@@ -176,7 +83,15 @@ func TestImplementsForStruct(t *testing.T) {
 func TestStructType_EmbeddedStruct(t *testing.T) {
 	typ := GetType(Dog{})
 	assert.True(t, typ.ToStructType().EmbeddedStruct(GetType(Animal{}).ToStructType()))
+	assert.True(t, typ.ToStructType().EmbeddedStruct(GetType(Cell{}).ToStructType()))
+	assert.False(t, typ.ToStructType().EmbeddedStruct(GetType(Dog{}).ToStructType()))
 	assert.Panics(t, func() {
 		typ.ToStructType().EmbeddedStruct(nil)
 	})
+}
+
+func TestStructType_NewInstance(t *testing.T) {
+	typ := GetType(Dog{})
+	instance := typ.ToStructType().NewInstance()
+	assert.NotNil(t, instance)
 }
